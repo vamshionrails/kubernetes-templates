e83a3a0bca7e463b8d8524932eec3964a9d90d0e
vamshionrails <vamshionrails@gmail.com>
2023-11-28 16:24:55 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,17 @@ TOKEN="$GITHUB_TOKEN"  # Access the GITHUB_TOKEN directly
 PRIVATE=false
 INITIALIZE_README=true
 
-# Create a new repository
-curl -u "$USERNAME:$TOKEN" https://api.github.com/user/repos -d "{\"name\":\"$REPO_NAME\",\"description\":\"$DESCRIPTION\",\"private\":$PRIVATE,\"auto_init\":$INITIALIZE_README}"
+# Set the repository name and description
+REPO_NAME=${REPO_NAME:-"your-repo-name"}
+DESCRIPTION=${DESCRIPTION:-"Your repository description"}
 
-echo "Repository created successfully: $USERNAME/$REPO_NAME"
+# Check if the DELETE_REPO variable is set
+if [[ "$DELETE_REPO" == "true" ]]; then
+  # Delete the repository
+  curl -X DELETE -u "$USERNAME:$TOKEN" "https://api.github.com/repos/$USERNAME/$REPO_NAME"
+  echo "Repository deleted successfully: $USERNAME/$REPO_NAME"
+else
+  # Create a new repository
+  curl -u "$USERNAME:$TOKEN" https://api.github.com/user/repos -d "{\"name\":\"$REPO_NAME\",\"description\":\"$DESCRIPTION\",\"private\":$PRIVATE,\"auto_init\":$INITIALIZE_README}"
+  echo "Repository created successfully: $USERNAME/$REPO_NAME"
+fi
