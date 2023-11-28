e37b28edfd58a83774d1e803c93ef01c04216b67
vamshionrails <vamshionrails@gmail.com>
2023-11-28 16:21:40 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,8 @@ USERNAME="$GITHUB_ACTOR"
 TOKEN="$GITHUB_TOKEN"  # Access the GITHUB_TOKEN directly
 
 # Repository details
-REPO_NAME="your-repo-name"
-DESCRIPTION="Your repository description"
 PRIVATE=false
 INITIALIZE_README=true
-
-# Get the directory of the script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create a new repository
 curl -u "$USERNAME:$TOKEN" https://api.github.com/user/repos -d "{\"name\":\"$REPO_NAME\",\"description\":\"$DESCRIPTION\",\"private\":$PRIVATE,\"auto_init\":$INITIALIZE_README}"
