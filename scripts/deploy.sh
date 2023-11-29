6d904c38f2ff0d47c6f17575bb40023a84a1588c
vamshi <vamshionrails@gmail.com>
2023-11-29 01:41:23 -0600

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,21 +26,21 @@ else
   curl -u "$USERNAME:$TOKEN" https://api.github.com/user/repos -d "{\"name\":\"$REPO_NAME\",\"description\":\"$DESCRIPTION\",\"private\":$PRIVATE,\"auto_init\":$INITIALIZE_README}"
   echo "Repository created successfully: $USERNAME/$REPO_NAME"
 
-  # git clone https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
-  # cd $REPO_NAME
+  git clone https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
+  cd $REPO_NAME
 
-  # git remote add origin https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
+  git remote add origin https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
 
-  # # Set the remote URL (origin)
-  # git remote set-url origin https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
+  # Set the remote URL (origin)
+  git remote set-url origin https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
 
-  # rm -rf * 
+  rm -rf * 
 
-  # # Add and commit changes
-  # git add .
-  # # git commit -m "Initializing"
-  # # git branch -M main
-  # # git push origin main
+  # Add and commit changes
+  git add .
+  git commit -m "Initializing"
+  git branch -M main
+  git push origin main
 
   echo "Additional folders and files added to the repository."
  
