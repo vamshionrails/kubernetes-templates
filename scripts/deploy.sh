00b1055f16d4fdc8eb210842b09d04d9633f3afd
vamshi <vamshionrails@gmail.com>
2023-11-29 02:02:20 -0600

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,41 +26,6 @@ else
   curl -u "$USERNAME:$TOKEN" https://api.github.com/user/repos -d "{\"name\":\"$REPO_NAME\",\"description\":\"$DESCRIPTION\",\"private\":$PRIVATE,\"auto_init\":$INITIALIZE_README}"
   echo "Repository created successfully: $USERNAME/$REPO_NAME"
 
-  git clone https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
-  cd $REPO_NAME
-
-  git remote add origin https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
-
-  # Set the remote URL (origin)
-  git remote set-url origin https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
-
-  rm -rf * 
-  cp -a ../template/. ../$REPO_NAME
-
-   echo '
-          {
-            "REPO_NAME": "'"$REPO_NAME"'",
-            "DESCRIPTION": "'"$DESCRIPTION"'",
-            "PRIVATE": '"$PRIVATE"',
-            "INITIALIZE_README": '"$INITIALIZE_README"',
-            "DELETE_REPO": '"$DELETE_REPO"',
-            "USERNAME": "'"$USERNAME"'",
-            "HELMFILE_PATH": "'"$HELMFILE_PATH"'",
-            "CHARTS_PATH": "'"$CHARTS_PATH"'",
-            "REPO_URL": "'"$REPO_URL"'",
-            "TEST": { "TEST_NAME": "VAMSHI" }
-            
-          }' > ${REPO_NAME}.json
-
-          # Print the content of the JSON file
-          cat $REPO_NAME.json
-
-  # Add and commit changes
-  git add .
-  git commit -m "Initialize..."
-  git branch -M main
-  git push origin main
-
-  echo "Additional folders and files added to the repository."
+ 
  
 fi
