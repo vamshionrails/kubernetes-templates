c110a93144fc44b6050c6b2c6194974db72a8be5
vamshionrails <vamshionrails@gmail.com>
2023-12-03 08:31:31 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,9 +22,30 @@ if [[ "$DELETE_REPO" == "true" ]]; then
   curl -L -X DELETE -u "$USERNAME:$TOKEN" "https://api.github.com/repos/$USERNAME/$REPO_NAME"
   echo "Repository deleted successfully: $USERNAME/$REPO_NAME"
 else
+  
   # Create a new repository
   curl -u "$USERNAME:$TOKEN" https://api.github.com/user/repos -d "{\"name\":\"$REPO_NAME\",\"description\":\"$DESCRIPTION\",\"private\":$PRIVATE,\"auto_init\":$INITIALIZE_README}"
   echo "Repository created successfully: $USERNAME/$REPO_NAME"
+
+  git clone https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
+  cd $REPO_NAME
+
+  git remote add origin https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
+
+  # Set the remote URL (origin)
+  git remote set-url origin https://$USERNAME:$TOKEN@github.com/$USERNAME/$REPO_NAME.git
+
+  rm -rf * 
+  cp -a ../template/. ../$REPO_NAME
+
+  # Add and commit changes
+  git add .
+  git commit -m "Initializing"
+  git branch -M main
+  git push origin main
+
+  echo "Additional folders and files added to the repository."
+ 
 
  
  
